65460cf8588a8fae17b8c14c4809867ce1ed75be
Cyrille Berger <cyrille.berger@liu.se>
2018-08-17 12:11:56 +0200

    add RosThread to RCL

--- Rcl/src/RosThread.cpp
+++ Rcl/src/RosThread.cpp
@@ -1,13 +1,16 @@
 #include "RosThread.h"
 
+#include <rcl/rcl.h>
+#include <rcl/error_handling.h>
+#include <rcl/time.h>
+
+#include <QCoreApplication>
 #include <QDebug>
 #include <QProcessEnvironment>
 
-#include <ros/ros.h>
-
-RosThread::RosThread()
+RosThread::RosThread() : m_rcl_node(rcl_get_zero_initialized_node())
 {
-
+  
 }
 
 RosThread* RosThread::instance()
@@ -15,32 +18,111 @@ RosThread* RosThread::instance()
   static RosThread* rt = nullptr;
   if(not rt)
   {
-    QString ros_arguments = QProcessEnvironment::systemEnvironment().value("ROS_ARGUMENTS");
-    ros::M_string map;
-    for(QString arg : ros_arguments.split(' '))
+    
+    QStringList ros_arguments = QProcessEnvironment::systemEnvironment().value("ROS_ARGUMENTS").split(' ');
+    QList<QByteArray> ros_argv_buffers;
+    char** ros_argv = new char*[ros_arguments.size()];
+    
+    for(int i = 0; i < ros_arguments.size(); ++i)
     {
-      QStringList split = arg.split(":=");
-      if(split.size() == 2)
-      {
-        map[split[0].toStdString()] = split[1].toStdString();
-      } else {
-        qWarning() << "Unhandled ros argument: " << arg;
-      }
+      QByteArray buffer = ros_arguments[i].toUtf8();
+      ros_argv[i] = buffer.data();
+      ros_argv_buffers.append(buffer);
     }
-    ros::init(map, "rosqml", ros::init_options::AnonymousName);
-    ros::start();
+    
+    delete[] ros_argv;
+    
+    if(rcl_init(ros_arguments.size(), ros_argv, rcl_get_default_allocator()) != RCL_RET_OK)
+    {
+      qFatal("Failed to initialize rmw implementation: %s", rcl_get_error_string_safe());
+    }
+    
+    QString ros_name = QProcessEnvironment::systemEnvironment().value("ROS_NAME");
+    if(ros_name.isEmpty())
+    {
+      ros_name = QString("qmlapp_%i").arg(QCoreApplication::applicationPid());
+    }
+
+    QString ros_namespace = QProcessEnvironment::systemEnvironment().value("ROS_NAMESPACE");
+
     rt = new RosThread();
+    rcl_node_options_t node_options = rcl_node_get_default_options();
+    if(rcl_node_init(&rt->m_rcl_node, qPrintable(ros_name), qPrintable(ros_namespace), &node_options))
+    {
+      qFatal("Failed to initialize node: %s", rcl_get_error_string_safe());
+    }
+    
   }
   return rt;
 }
 
+void RosThread::registerClient(ServiceClient* _client)
+{
+  QMutexLocker l(&m_mutex);
+  m_clients.append(_client);
+}
+
+void RosThread::unregisterClient(ServiceClient* _client)
+{
+  QMutexLocker l(&m_mutex);
+  m_clients.removeAll(_client);
+}
+
+void RosThread::registerSubscriber(Subscriber* _subscriber)
+{
+  QMutexLocker l(&m_mutex);
+  m_subscribers.append(_subscriber);
+}
+
+void RosThread::unregisterSubscriber(Subscriber* _subscriber)
+{
+  QMutexLocker l(&m_mutex);
+  m_subscribers.removeAll(_subscriber);
+}
+
+
 void RosThread::run()
 {
   m_startTime = now();
-  ros::spin();
+  while(true)
+  {
+    {
+      QMutexLocker l(&m_mutex);
+      // Handle subscription
+      for(Subscriber* sub : m_subscribers)
+      {
+        qFatal("unimplemented");
+        Q_UNUSED(sub);
+      }
+    }
+    {
+      QMutexLocker l(&m_mutex);
+      // Handle client
+      for(ServiceClient* cl : m_clients)
+      {
+        qFatal("unimplemented");
+        Q_UNUSED(cl);
+      }
+    }
+  }
 }
 
 quint64 RosThread::now() const
 {
-  return ros::Time::now().toNSec();
+  rcl_allocator_t allocator = rcl_get_default_allocator();
+  rcl_clock_t clock;
+  if(rcl_clock_init(RCL_SYSTEM_TIME, &clock, &allocator) != RCL_RET_OK)
+  {
+    qFatal("Failed to initialize time point.");
+  }
+  rcl_time_point_t ns;
+  if(rcl_clock_get_now(&clock, &ns) != RCL_RET_OK)
+  {
+    qFatal("Failed to get current time.");
+  }
+  if(rcl_clock_fini(&clock) != RCL_RET_OK)
+  {
+    qFatal("Failed to finalize clock.");
+  }
+  return ns.nanoseconds;
 }
--- Rcl/src/RosThread.h
+++ Rcl/src/RosThread.h
@@ -1,7 +1,14 @@
 #ifndef ROSTHREAD_H
 #define ROSTHREAD_H
 
+#include <QMutex>
 #include <QThread>
+#include <QThreadPool>
+
+#include <rcl/node.h>
+
+class Subscriber;
+class ServiceClient;
 
 class RosThread : public QThread
 {
@@ -10,10 +17,20 @@ public:
   static RosThread* instance();
   quint64 now() const;
   quint64 startTime() const { return m_startTime; }
+  rcl_node_t* rclNode() { return &m_rcl_node; }
+  void registerSubscriber(Subscriber* _subscriber);
+  void unregisterSubscriber(Subscriber* _subscriber);
+  void registerClient(ServiceClient* _client);
+  void unregisterClient(ServiceClient* _client);
 protected:
   void run();
 private:
+  rcl_node_t m_rcl_node;
   quint64 m_startTime;
+  QThreadPool m_threadPool;
+  QMutex m_mutex;
+  QList<Subscriber*>    m_subscribers;
+  QList<ServiceClient*> m_clients;
 };
 
 #endif // ROSTHREAD_H
